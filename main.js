b84ae84a3ab2a46628dafbb5ea6d4c6438a178d6
Bishal Hasan Bhuiyan <bishalqx680@gmail.com>
2025-07-21 00:38:46 +0600

    minor update

--- main.js
+++
@@ -1,9 +0,0 @@
-function load() {
-    const from_yearElement = document.getElementById("from_year");
-    const current_yearElement = document.getElementById("current_year");
-
-    const date = new Date();
-
-    from_yearElement.textContent = 2023;
-    current_yearElement.textContent = date.getFullYear();
-}
---
+++ other/README.md
@@ -0,0 +1,3 @@
+# Don't Add/Delete `files` here
+
+> This dir files used for old stuff
